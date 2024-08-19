50754bab14b3c5f94f0011e67b03eeb37730190e
Liam Seymour <spaceturatle@gmail.com>
2024-08-19 08:58:19 -0500

    iuouhk

---
+++ All about me/LiamView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  All about me
+//
+//  Created by Liam K. Seymour on 8/19/24.
+//
+
+import SwiftUI
+
+struct LiamView: View {
+    var body: some View {
+        Text("Liam")
+    }
+}
+
+#Preview {
+    LiamView()
+}
