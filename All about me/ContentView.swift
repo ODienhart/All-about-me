92baf572652c41e9b37ccdc8d0d4119c9fa3f179
Oliver Dienhart <odienhart@icloud.com>
2024-08-19 08:59:06 -0500

    Dropdown

--- All about me/ContentView.swift
+++ All about me/ContentView.swift
@@ -1,24 +1,35 @@
-//
-//  ContentView.swift
-//  All about me
-//
-//  Created by Oliver W. Dienhart on 8/15/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: String = "Select an item"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("oli")
+            
+            Menu {
+                Button("Option 1") {
+                    selectedItem = "Option 1"
+                }
+                Button("Option 2") {
+                    selectedItem = "Option 2"
+                }
+                Button("Option 3") {
+                    selectedItem = "Option 3"
+                }
+            } label: {
+                Label(selectedItem, systemImage: "chevron.down")
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
+
