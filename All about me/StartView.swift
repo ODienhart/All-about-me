3f99c6d1a3a2293a2465e9f914544a0c50603238
Liam Seymour <spaceturatle@gmail.com>
2024-08-19 09:22:20 -0500

    Added Liam View and Start View. Navigation Links need to be made nicer

---
+++ All about me/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  All about me
+//
+//  Created by Liam K. Seymour on 8/19/24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
