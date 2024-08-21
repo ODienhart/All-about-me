559bc63908e9495a89d29ae7a204bc050a2d0c24
Liam Seymour <spaceturatle@gmail.com>
2024-08-21 09:11:55 -0500

    uhio

--- All about me/StartView.swift
+++ All about me/StartView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct StartView: View {
     var body: some View {
-        Text("All About e")
+        Text("All About Me")
             .font(.custom("Al Nile", size: 30))
         Divider()
         NavigationView(content: {
