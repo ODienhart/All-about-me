b92783372314afe52f8c8ef0e026da0632464b0a
Oliver Dienhart <odienhart@icloud.com>
2024-08-21 08:27:37 -0500

    liam weird

--- All about me/ContentView.swift
+++ All about me/ContentView.swift
@@ -1,30 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectedItem: String = "Select an item"
-
     var body: some View {
         VStack {
-            
-            Menu {
-                Button("Option 1") {
-                    selectedItem = "Option 1"
-                }
-                Button("Option 2") {
-                    selectedItem = "Option 2"
-                }
-                Button("Option 3") {
-                    selectedItem = "Option 3"
-                }
-            } label: {
-                Label(selectedItem, systemImage: "chevron.down")
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(8)
+            Text("Oliver Dienhart")
+                .font(.custom("Palatino", size: 50))
+            Text("Year: Junior | 26")
+                .font(.custom("Palatino", size: 25))
+            Text("Sport: Soccer")
+                .font(.custom("Palatino", size: 25))
+            Text("Fun Fact: ")
+                .font(.custom("Palatino", size: 25))
+            Text("Two Truths and a Lie")
+                .font(.custom("Palatino", size: 35))
+            Text("1) I've broken 4 bones. \n2) I have been to 9 countries( i think i cant count)  \n3) ")
+                .font(.custom("Palatino", size: 30))
             }
         }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
